eee64b51fbaedfdf3ac80a7c87d662a0c54f9675
sa_me_des <eightsbinary@gmail.com>
2024-10-13 01:55:31 +0700

    add: prettierignore, .prettierrc, update: eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: { ...globals.browser },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    files: ['src/**/*.js'],
+    ignores: [
+      '**/*.config.js',
+      '!**/eslint.config.js',
+      '**/*.config.mjs',
+      '!**/eslint.config.mjs',
+    ],
+    rules: {},
+  },
   pluginJs.configs.recommended,
 ];
