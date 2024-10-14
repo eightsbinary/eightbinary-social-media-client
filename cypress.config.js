f467ebecde081049ae58276c6d688dd9e0eb9908
sa_me_des <eightsbinary@gmail.com>
2024-10-14 14:01:21 +0700

    add: config to integrate eslint and cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Set your application's base URL
+    baseUrl: 'http://localhost:8080', // Set your application's base URL
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
   },
   viewportWidth: 1280,
   viewportHeight: 720,
@@ -20,6 +17,6 @@ export default defineConfig({
   },
   env: {
     // Environment variables
-    API_URL: 'https://api.example.com',
+    API_URL: 'https://nf-api.onrender.com/api/v1',
   },
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,44 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginCypress from 'eslint-plugin-cypress/flat'
 
 export default [
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.jest, ...globals.node },
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-    },
+    // extends: ['plugin:cypress/recommended', 'plugin:mocha/recommended'],
     files: ['src/**/*.js'],
     ignores: [
       '!node_modules/', // unignore `node_modules/` directory
       'node_modules/*', // ignore its content
     ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: pluginCypress,
     rules: {
+      ...pluginCypress.configs.recommended.rules,
       'comma-dangle': ['error', 'always-multiline'],
       'no-console': 'warn',
       'no-unused-vars': 'warn',
+      'cypress/assertion-before-screenshot': 'warn',
+      'cypress/no-assigning-return-values': 'error',
+      'cypress/no-async-before': 'error',
+      'cypress/no-async-tests': 'error',
+      'cypress/no-debug': 'error',
+      'cypress/no-force': 'warn',
+      'cypress/no-pause': 'error',
+      'cypress/no-unnecessary-waiting': 'error',
       indent: ['error', 2],
       quotes: ['error', 'single'],
     },
   },
+  pluginCypress.configs.globals,
   pluginJs.configs.recommended,
 ];
